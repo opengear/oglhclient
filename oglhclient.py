45819d337c8a377917a9fee38ccb7c44675a0eb9
Robert W <robert.waldie@opengear.com>
2017-10-10 09:42:04 +0000

    Misc minor fixups
    - Fix typos
    - Update repository location

--- oglhclient.py
+++ oglhclient.py
@@ -40,7 +40,7 @@ class LighthouseApiClient:
         if not (self.url and self.username and self.password):
             raise RuntimeError("""
             Some of the required environment variables are not set, please refer
-            to the documentation: https://github.com/opengeardev/oglhclient
+            to the documentation: https://github.com/opengear/oglhclient
             """)
 
         requests.packages.urllib3.disable_warnings()
@@ -70,7 +70,7 @@ class LighthouseApiClient:
         
         /system
             /time
-        it is helpefull for parsing the raml file
+        it is helpful for parsing the raml file
         """
         top_paths = [p for p in raml.keys() if re.match('^\/', p)]
         for p in top_paths:
